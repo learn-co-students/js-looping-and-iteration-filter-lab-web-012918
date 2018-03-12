11971aa9b8ab2bf4e01545e05c79f696cffdf959
scott <you@example.comitsmescottb123@gmail.com>
2018-03-11 22:13:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+
+function findMatching(arr, str) {
+  return arr.filter(function (ele) {return ele.toLowerCase() === str.toLowerCase();});
+}
+
+function fuzzyMatch(arr, str) {
+  return arr.filter(function (ele) {return ele.startsWith(str)});
+}
+
+function matchName(arr, str) {
+  return arr.filter(function (ele) {return ele.name === str});
+}
