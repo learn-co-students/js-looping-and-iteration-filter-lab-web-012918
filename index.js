f7eeb49e261018e95e6a96121a7f13b281d7dee7
gilsunshine <gilsunshine@gmail.com>
2018-03-11 20:42:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, string){
+  return drivers.filter(function(name){return name.toUpperCase() === string.toUpperCase();})
+};
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(function(name){return name.toUpperCase().startsWith(string.toUpperCase());})
+};
+
+function matchName(drivers, string){
+  return drivers.filter(function(driver){return driver.name.toUpperCase() === string.toUpperCase();})
+};
