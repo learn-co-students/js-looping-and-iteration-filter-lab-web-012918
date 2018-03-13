6f9581b0372dbbfe21b78593f45342a47385b2db
cs-j <cj@mail.co.de>
2018-03-13 08:56:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function(name) { return name.toUpperCase() === string.toUpperCase(); });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(name) { return name.slice(0, 1) === string.slice(0, 1); });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function(driverName) { return driverName.name === string; });
+}
