26083b63215454f77779bfac69b1767b7ce7b9be
cs-j <cj@mail.co.de>
2018-03-13 08:58:55 -0400

    Done.

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function fuzzyMatch(drivers, string) {
 }
 
 function matchName(drivers, string) {
-  return drivers.filter(function(driverName) { return driverName.name === string; });
+  return drivers.filter(function(name) { return name.name === string; });
 }
