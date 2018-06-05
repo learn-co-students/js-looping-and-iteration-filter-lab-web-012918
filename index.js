9bca3bd9ae48e9ecce81fd2312c328cf26117598
github username <github email address>
2018-06-05 00:39:37 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file
+findMatching = (a,s) => {
+  return a.filter(el =>
+    el.toLowerCase() === s.toLowerCase()
+  )
+  // Filter, and find all elements that match the string (matched as lowercased).
+}
+
+fuzzyMatch = (a,s) => {
+ return a.filter(el =>
+   el.startsWith(s)
+ )
+ // Filter, and find all elements that start with the string.
+}
+
+matchName = (a,s) => {
+  return a.filter(el =>
+    el.name === s
+  )
+  // Filter, and find all objects who's name key matches the string.
+}
